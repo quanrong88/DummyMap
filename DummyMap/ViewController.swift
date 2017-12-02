d1cd9b6a3b71df30b45926797a415b8bd14b7807
Ta Minh Quan <taquan88bk@gmail.com>
2017-12-02 14:56:09 +0700

    Initial Commit

---
+++ DummyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DummyMap
+//
+//  Created by Ta Minh Quan on 02/12/2017.
+//  Copyright © 2017 Ta Minh Quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
