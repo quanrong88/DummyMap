3f74c6a3efe927de71bec66945d65a3c940246f7
Ta Minh Quan <taquan88bk@gmail.com>
2017-12-03 11:06:18 +0700

    - add feed view controller

---
+++ DummyMap/FeedVC.swift
@@ -0,0 +1,36 @@
+//
+//  FeedVC.swift
+//  DummyMap
+//
+//  Created by Ta Minh Quan on 02/12/2017.
+//  Copyright © 2017 Ta Minh Quan. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    @IBOutlet weak var feedCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
